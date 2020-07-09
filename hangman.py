8523128d6b6dd1e0ff2b484b9108cf4b777d5780
taka135711 <68044312+taka135711@users.noreply.github.com>
2020-07-09 20:29:43 +0900

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,14 +1,14 @@
 def hangman(word) :
     wrong = 0
     stages = [' ',
-                     '________        ',
-                     ' |                    ',
-                     ' |          |         ',
-                     ' |         0         ',
-                     ' |       / | \       ',
-                     ' |       /   \       ',
-                     ' |                    ',
-                     ]
+              '________             ',
+              ' |                   ',
+              ' |         |         ',
+              ' |         0         ',
+              ' |       / | \       ',
+              ' |       /   \       ',
+              ' |                   ',
+              ]
     rletters = list(word)
     board = ['_'] * len(word)
     win = False
